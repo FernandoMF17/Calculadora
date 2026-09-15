214a2125066d2e273ee45299824fd3961d307998
Yasmin <yasminkadiaz.email>
2026-09-15 13:49:30 -0400

    Agregar potencia

--- ejercicio.py
+++ ejercicio.py
@@ -6,3 +6,12 @@ def resta(a,b):
 
 def multiplicacion(a,b):
     return a*b
+
+def potencia(a, b):
+    resul = 1
+    for i in range(b):
+        resul=resul*a
+        return resul
+assert potencia(2,3)==8
+assert potencia(5,2)==25
+assert potencia(10,0)==1
