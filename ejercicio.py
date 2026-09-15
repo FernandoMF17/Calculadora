af08066e93d1c62aa329880821895cc654b73a8d
Yasmin <yasminkadiaz.email>
2026-09-15 13:57:48 -0400

    Potencia agregada

--- ejercicio.py
+++ ejercicio.py
@@ -8,10 +8,4 @@ def multiplicacion(a,b):
     return a*b
 
 def potencia(a, b):
-    resul = 1
-    for i in range(b):
-        resul=resul*a
-        return resul
-assert potencia(2,3)==8
-assert potencia(5,2)==25
-assert potencia(10,0)==1
+    return pow(a, b)
